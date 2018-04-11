d7b40503d97f86fc7aff0b2b4be53de4e7d32c1a
Eryk Wdowiak <34685946+ewdowiak@users.noreply.github.com>
2018-04-10 21:15:13 -0400

    Add files via upload

---
+++ cgi-src/make_cchiu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##  "make_cchiu.sh" -- a quick little script to assemble the pieces
+##  Copyright (C) 2018 Eryk Wdowiak
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##
+
+./mk_dieli-dicts.pl
+./mk_dieli-edits.pl
+./mk_verb-tools.pl
+./mk_verb-notes.pl
